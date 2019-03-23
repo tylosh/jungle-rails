f8465a413c3a6ef1e7ccb01acf00dd2af2e8daae
Tyler <tyler.tomczyk@gmail.com>
2019-03-23 19:13:55 +0000

    step 2: validations to model added for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ActiveRecord::Base
     belongs_to :product
 
+    #required for a review to created
+    validates :product_id, presence: true
+    validates :user_id, presence: true
+    validates :description, presence: true
+    validates :rating, presence: true
+
 end
