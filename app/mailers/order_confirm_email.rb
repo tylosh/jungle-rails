babc2cd0e1d17a9650811b0049140407caf544c4
Tyler <tyler.tomczyk@gmail.com>
2019-03-25 20:30:56 +0000

    order id in subject line

--- app/mailers/order_confirm_email.rb
+++ app/mailers/order_confirm_email.rb
@@ -2,7 +2,7 @@ class OrderConfirmEmail < ApplicationMailer
 
     def order_confirm_email order
         @order = order
-        mail(to: order.email, subject: 'Order Receipt')
+        mail(to: order.email, subject: "Jungle Order ID:  #{order.id} | Thank you for your order!") 
     end    
 
 end
