85d404ba5c856bf08d3dad51d8b3e5dfc80984aa
Tyler <tyler.tomczyk@gmail.com>
2019-03-25 03:27:15 +0000

    step 11 completed with review being posted/rendered

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,7 +6,25 @@ class ReviewsController < ApplicationController
 
     
     def create
-        raise "Yay, I'm here!"
-      end
+        # create and save the review to the database
+        puts '^^^^^^^^^'
+        puts allowed_params
+        @review = Review.create!(allowed_params)
+
+        if @review.save
+            redirect_to :back
+          else
+            redirect_to :back 
+        end
+    end
+
+
+    private
+
+    def allowed_params
+        puts '*********************'
+        puts params
+        params.require(:review).permit(:product_id, :user_id, :rating, :description)
+    end
 
   end
